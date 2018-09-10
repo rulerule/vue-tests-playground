bff981a56f4077a86a010f16e636556f21dbf283
INNOVMAKERS\andre.goncalves <andre.goncalves@inm.pt>
2018-09-10 18:14:43 +0100

    added first part of guide project

--- src/components/secondPart/monsterGameOfficial.vue
+++ src/components/secondPart/monsterGameOfficial.vue
@@ -5,31 +5,31 @@
           <div class="small-6 columns">
               <h1 class="text-center">YOU</h1>
               <div class="healthbar">
-                  <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;">
-
+                  <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;" :style="{width: playerHealth + '%'}">
+                    {{ playerHealth }}
                   </div>
               </div>
           </div>
           <div class="small-6 columns">
               <h1 class="text-center">MONSTER</h1>
               <div class="healthbar">
-                  <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;">
-
+                  <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;" :style="{width: monsterHealth + '%'}">
+                    {{monsterHealth}}
                   </div>
               </div>
           </div>
       </section>
-      <section class="row controls">
+      <section class="row controls" v-if="!gameIsRunning">
           <div class="small-12 columns">
-              <button id="start-game">START NEW GAME</button>
+              <button id="start-game" @click="startGame" >START NEW GAME</button>
           </div>
       </section>
-      <section class="row controls">
+      <section class="row controls" v-else>
           <div class="small-12 columns">
-              <button id="attack">ATTACK</button>
-              <button id="special-attack">SPECIAL ATTACK</button>
-              <button id="heal">HEAL</button>
-              <button id="give-up">GIVE UP</button>
+              <button id="attack" @click="attack">ATTACK</button>
+              <button id="special-attack" @click="specialAttack">SPECIAL ATTACK</button>
+              <button id="heal" @click="heal">HEAL</button>
+              <button id="give-up" @click="giveUp">GIVE UP</button>
           </div>
       </section>
       <section class="row log">
@@ -51,9 +51,75 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      gameIsRunning: false,
+      turns: []
+    }
   },
   methods: {
+    startGame () {
+      this.gameIsRunning = true
+      this.playerHealth = 100
+      this.monsterHealth = 100
+    },
+    attack () {
+      let damage = this.calculateDamage(3, 10)
+      this.monsterHealth -= damage
+      this.turns.unshift({
+        isPlayer: true,
+        text: `Player hits Monster for ${damage}`
+      })
+      if (this.checkWin()) return
+      this.monsterAttacks()
+    },
+    specialAttack () {
+      this.monsterHealth -= this.calculateDamage(10, 20)
+      if (this.checkWin()) return
+      this.monsterAttacks()
+    },
+    heal () {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10
+      } else {
+        this.playerHealth = 100
+      }
+      this.monsterAttacks()
+    },
+    giveUp () {
+      this.gameIsRunning = false
+    },
+    calculateDamage (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min)
+    },
+    monsterAttacks () {
+      let damage = this.calculateDamage(5, 15)
+      this.playerHealth -= damage
+      this.turns.unshift({
+        isPlayer: false,
+        text: `Monster hits Player for ${damage}`
+      })
+      this.checkWin()
+    },
+    checkWin () {
+      if (this.monsterHealth <= 0) {
+        if (confirm('you won! new game?')) {
+          this.startGame()
+        } else {
+          this.gameIsRunning = false
+        }
+        return true
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You Lost! Play Again?')) {
+          this.startGame()
+        } else {
+          this.gameIsRunning = false
+        }
+        return true
+      }
+      return false
+    },
     backToList () {
       this.$router.go(-1)
     }
