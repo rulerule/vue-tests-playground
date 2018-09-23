20b1596b7b423df68177d0b45cc468b14d367ac6
rulerule <andre.s.goncalves@tecnico.ulisboa.pt>
2018-09-23 22:11:55 +0100

    directives exercises

--- src/components/eightPart/exercise10.vue
+++ src/components/eightPart/exercise10.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="exercise-10">
+    <button v-customOn:click="clicked" class="btn btn-primary"> Click Me </button>
+    <hr>
+    <div v-customOn:mouseenter="mouseEnter" v-customOn:mouseleave="mouseLeave" style="width:100px; height:100px; background-color:pink"></div>
     <GoBack></GoBack>
   </div>
 </template>
@@ -12,6 +15,29 @@ export default {
   },
   components: {
     GoBack
+  },
+  directives: {
+    customOn: {
+      bind (el, binding) {
+        // el.onclick = function () {
+        //   binding.value()
+        // }
+        const type = binding.arg
+        const fn = binding.value
+        el.addEventListener(type, fn)
+      }
+    }
+  },
+  methods: {
+    clicked () {
+      alert('I was clicked')
+    },
+    mouseEnter () {
+      alert('mouse entered')
+    },
+    mouseLeave () {
+      alert('mouse left')
+    }
   }
 }
 </script>
